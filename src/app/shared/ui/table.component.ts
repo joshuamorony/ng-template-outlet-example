249133abbe65f84cc9f937b4fe6ca0b4fdccbdb2
Joshua Morony <joshua.morony@gmail.com>
2022-07-31 13:59:17 +0930

    add styles

--- src/app/shared/ui/table.component.ts
+++ src/app/shared/ui/table.component.ts
@@ -11,22 +11,26 @@ import {
   selector: 'app-table',
   template: `
     <table>
-      <tr>
-        <ng-container
-          *ngTemplateOutlet="
-            headers || defaultHeaderTemplate;
-            context: { $implicit: data }
-          "
-        ></ng-container>
-      </tr>
-      <tr *ngFor="let row of data">
-        <ng-container
-          *ngTemplateOutlet="
-            rows || defaultRowTemplate;
-            context: { $implicit: row }
-          "
-        ></ng-container>
-      </tr>
+      <thead>
+        <tr>
+          <ng-container
+            *ngTemplateOutlet="
+              headers || defaultHeaderTemplate;
+              context: { $implicit: data }
+            "
+          ></ng-container>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let row of data">
+          <ng-container
+            *ngTemplateOutlet="
+              rows || defaultRowTemplate;
+              context: { $implicit: row }
+            "
+          ></ng-container>
+        </tr>
+      </tbody>
     </table>
 
     <!-- If no template is provided use keys as headers and display all values -->
@@ -38,6 +42,34 @@ import {
       <td *ngFor="let row of row | keyvalue">{{ row.value }}</td>
     </ng-template>
   `,
+  styles: [
+    `
+      ::ng-deep table {
+        width: 100%;
+        margin: 2rem 0;
+        border-collapse: collapse;
+        font-family: sans-serif;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+
+        thead {
+          tr {
+            background-color: #dd0031;
+            color: #ffffff;
+            text-align: left;
+          }
+        }
+
+        tbody tr:hover {
+          background-color: #f6f6f6;
+        }
+
+        th,
+        td {
+          padding: 1rem;
+        }
+      }
+    `,
+  ],
 })
 export class TableComponent {
   @Input() data!: any[];
